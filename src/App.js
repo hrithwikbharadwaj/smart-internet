3f5080266ce57b540d6cd030a57059934b7600fa
hrithwik6986 <4ni17is028_a@nie.ac.in>
2021-09-22 23:41:02 +0530

    UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState,useEffect } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [activeIndex, setActiveIndex] = useState("Marketing");
+  const [selectedTag,setSelectedTag] = useState("Marketing")
+  useEffect(()=>{
+    function fetchData () {
+     
+      fetch(`https://functions-cloud-hsb.harperdbcloud.com/api/resources/${selectedTag}`, {
+    method: 'GET', // or 'PUT'
+   
+    
+  })
+  .then(response => response.json())
+  .then(data => {
+    setItems(data);
+  })
+    }
+    fetchData()
+    
+   
+  },[selectedTag]);
+  
+  
+
+  const tags=["Marketing","Product","Startups","Crypto","AI"];
+
+
+
+function ChangeResource(tag){
+ 
+  setSelectedTag(tag);
+  setActiveIndex(tag);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <> 
+    <div className="row">
+     <div className="Boxtitle">
+      <h1> Curated List of Best Articles and Resources</h1>
+     </div>
+     </div>
+
+     <div className="Boxcontent">
+   
+     {tags.map(tag=>(
+       <div onClick={() => ChangeResource(tag)} key={tag} className={activeIndex === tag ? "sectionType active" : "sectionType"}> 
+       <h3  > {tag} </h3>
+       </div>
+     ))}
+
+     </div>
+<div className="gridWrap"> 
+ {items.map((item,key)=>{
+
+  
+    return(
+        <div key={item.title} className="GridView">
+     <a href={item.url} >
+     <img   style={{height:"200px",width:"400px",borderRadius:"30px"}} src={item.cover} alt="" /> 
+     <h3 style={{color:"white",width:"100%",maxWidth:"400px",textAlign:"center",display:"absolute"}}>  {item.title}  </h3>
+     </a>
+  </div> ) }
+  )} 
+    
     </div>
+
+    
+
+    
+     
+    </>
   );
 }
 
